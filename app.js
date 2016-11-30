e90a9ba66755d8df978462aa1265f18524ab78c2
Lord Chika Onyebueke <onye108@gmail.com>
2016-11-29 19:10:52 -0500

    all prompts complete

--- app.js
+++ app.js
@@ -1,2 +1,63 @@
+'use strict';
 
-alert('I am alive!!');
+// The questions in the guessing game must require a mix of yes/no answers, and user input must accept either y/n or yes/no questions, with either .toUpperCase() or .toLowerCase() used to validate the user input and accommodate users entering all-caps Y/N or YES/NO answers, too.
+
+var response1 = prompt('Does Chika have any dogs?');
+
+if (response1.toLowerCase() === 'yes' || response1.toLowerCase() === 'y') {
+  alert('Yes, Chika has 1 dog');
+} else if (response1 === 'no' || response1 === 'n')
+{
+  alert('No, I dont think you know Chika');
+}else {
+  alert('What are you even talking about');
+}
+console.log('Chika has a dog name Samson');
+
+var response2 = prompt('Does Chika have any siblings?');
+
+if (response2.toLowerCase() === 'yes' || response2.toLowerCase() === 'y') {
+  alert('Yes, 2 sisters and 2 brothers');
+} else if (response2 === 'no' || response2 === 'n')
+{
+  alert('No, I dont think you know Chika');
+}else {
+  alert('Say What?');
+}
+console.log('Lord is the 2nd oldest child of 5');
+
+var response3 = prompt('Does Chika like to fish?');
+
+if (response3.toLowerCase() === 'yes' || response3.toLowerCase() === 'y') {
+  alert('Correct, esepcially salt water');
+} else if (response2 === 'no' || response2 === 'n')
+{
+  alert('No, I dont think you know Chika');
+}else {
+  alert('Say What?');
+}
+console.log('Chika absolutely loves to fish');
+
+var response4 = prompt('Have you met Chika before?');
+
+if (response4.toLowerCase() === 'yes' || response4.toLowerCase() === 'y') {
+  alert('Cool guy right?');
+} else if (response2 === 'no' || response2 === 'n')
+{
+  alert('You\'re missing out.');
+}else {
+  alert('Say What?');
+}
+console.log('Chika changed my life!');
+
+var response5 = prompt('Do you also believe that Chika will be a mogul ?');
+
+if (response5.toLowerCase() === 'yes' || response5.toLowerCase() === 'y') {
+  alert('He\'s a jack of many trades huh?');
+} else if (response2 === 'no' || response2 === 'n')
+{
+  alert('Oh ye of little faith!');
+}else {
+  alert('Say What?');
+}
+console.log('Chika will end poverty in his family!');

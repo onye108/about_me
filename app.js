77c0b261cf4614402b9fabc656ec0bb09bafa1cf
DARCY MCCABE <DARMCCA@GMAIL.COM>
2016-12-01 16:26:11 -0800

    Added correctAnswer counter to tally total number correct

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 'use strict';
 
+var correctAnswer = 0;
+
 function answer1(){
   var response1 = prompt('Does Chika have any dogs?');
   if (response1.toLowerCase() === 'yes' || response1.toLowerCase() === 'y') {
     alert('Yes, Chika has 1 dog');
+    correctAnswer += 1;
   } else if (response1 === 'no' || response1 === 'n')
 {
     alert('No, I dont think you know Chika');
@@ -19,6 +22,7 @@ function answer2(){
   var response2 = prompt('Does Chika have any siblings?');
   if (response2.toLowerCase() === 'yes' || response2.toLowerCase() === 'y') {
     alert('Yes, 2 sisters and 2 brothers');
+    correctAnswer += 1;
   } else if (response2 === 'no' || response2 === 'n'){
     alert('No, I dont think you know Chika');
   }else {
@@ -33,6 +37,7 @@ function answer3(){
   var response3 = prompt('Does Chika like to fish?');
   if (response3.toLowerCase() === 'yes' || response3.toLowerCase() === 'y') {
     alert('Correct, especially salt water');
+    correctAnswer += 1;
   }else if (response2.toLowerCase() === 'no' || response2.toLowerCase() === 'n')
 {
     alert('No, I dont think you know Chika');
@@ -48,6 +53,7 @@ function answer4(){
   var response4 = prompt('Have you met Chika before?');
   if (response4.toLowerCase() === 'yes' || response4.toLowerCase() === 'y') {
     alert('Cool guy right?');
+    correctAnswer += 1;
   }else if (response2 === 'no' || response2 === 'n')
 {alert('You\'re missing out.');
   }else {
@@ -60,17 +66,17 @@ answer4()
 
 function answer5() {
   var response5 = prompt('Does Chika enjoy being outdoors?');
-
   if (response5.toLowerCase() === 'yes' || response5.toLowerCase() === 'y') {
     alert('Mr.National Geographic right?');
-    }else if (response2 === 'no' || response2 === 'n')
+    correctAnswer += 1;
+  }else if (response2 === 'no' || response2 === 'n')
   {
     alert('Think again');
   }else {
     alert('Say What?');
   }
   console.log('Chika loves the outdoors!');
-  }
+}
 
 answer5()
 

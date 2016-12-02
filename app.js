1ff7e108884d34f6bbe1d9b3be0fb4da6793be04
DARCY MCCABE <DARMCCA@GMAIL.COM>
2016-12-01 16:16:52 -0800

    Most indentation issues resolved

--- app.js
+++ app.js
@@ -1,84 +1,76 @@
 'use strict';
 
 function answer1(){
-var correctAnswer = 0;
-
-var response1 = prompt('Does Chika have any dogs?');
-
-if (response1.toLowerCase() === 'yes' || response1.toLowerCase() === 'y') {
-  alert('Yes, Chika has 1 dog');
-} else if (response1 === 'no' || response1 === 'n')
+  var response1 = prompt('Does Chika have any dogs?');
+  if (response1.toLowerCase() === 'yes' || response1.toLowerCase() === 'y') {
+    alert('Yes, Chika has 1 dog');
+  } else if (response1 === 'no' || response1 === 'n')
 {
-  alert('No, I dont think you know Chika');
-}else {
-  alert('What are you even talking about');
-}
-console.log('Chika has a dog name Samson');
+    alert('No, I dont think you know Chika');
+  }else {
+    alert('What are you even talking about');
+  }
+  console.log('Chika has a dog name Samson');
 }
 
 answer1()
 
 function answer2(){
-var response2 = prompt('Does Chika have any siblings?');
-
-if (response2.toLowerCase() === 'yes' || response2.toLowerCase() === 'y') {
-  alert('Yes, 2 sisters and 2 brothers');
-} else if (response2 === 'no' || response2 === 'n')
-{
-  alert('No, I dont think you know Chika');
-}else {
-  alert('Say What?');
-}
-console.log('Lord is the 2nd oldest child of 5');
+  var response2 = prompt('Does Chika have any siblings?');
+  if (response2.toLowerCase() === 'yes' || response2.toLowerCase() === 'y') {
+    alert('Yes, 2 sisters and 2 brothers');
+  } else if (response2 === 'no' || response2 === 'n'){
+    alert('No, I dont think you know Chika');
+  }else {
+    alert('Say What?');
+  }
+  console.log('Lord is the 2nd oldest child of 5');
 }
 
 answer2()
 
 function answer3(){
-var response3 = prompt('Does Chika like to fish?');
-
-if (response3.toLowerCase() === 'yes' || response3.toLowerCase() === 'y') {
-  alert('Correct, esepcially salt water');
-} else if (response2 === 'no' || response2 === 'n')
+  var response3 = prompt('Does Chika like to fish?');
+  if (response3.toLowerCase() === 'yes' || response3.toLowerCase() === 'y') {
+    alert('Correct, especially salt water');
+  }else if (response2.toLowerCase() === 'no' || response2.toLowerCase() === 'n')
 {
-  alert('No, I dont think you know Chika');
-}else {
-  alert('Say What?');
-}
-console.log('Chika absolutely loves to fish');
+    alert('No, I dont think you know Chika');
+  }else {
+    alert('Say What?');
+  }
+  console.log('Chika absolutely loves to fish');
 }
 
 answer3()
 
 function answer4(){
-var response4 = prompt('Have you met Chika before?');
-
-if (response4.toLowerCase() === 'yes' || response4.toLowerCase() === 'y') {
-  alert('Cool guy right?');
-} else if (response2 === 'no' || response2 === 'n')
-{
-  alert('You\'re missing out.');
-}else {
-  alert('Say What?');
-}
-console.log('Chika changed my life!');
+  var response4 = prompt('Have you met Chika before?');
+  if (response4.toLowerCase() === 'yes' || response4.toLowerCase() === 'y') {
+    alert('Cool guy right?');
+  }else if (response2 === 'no' || response2 === 'n')
+{alert('You\'re missing out.');
+  }else {
+    alert('Say What?');
+  }
+  console.log('Chika changed my life!');
 }
 
 answer4()
 
 function answer5() {
-var response5 = prompt('Does Chika enjoy being outdoors?');
+  var response5 = prompt('Does Chika enjoy being outdoors?');
 
-if (response5.toLowerCase() === 'yes' || response5.toLowerCase() === 'y') {
-  alert('Mr.National Geographic right?');
-} else if (response2 === 'no' || response2 === 'n')
-{
-  alert('Think again');
-}else {
-  alert('Say What?');
-}
-console.log('Chika loves the outdoors!');
-}
+  if (response5.toLowerCase() === 'yes' || response5.toLowerCase() === 'y') {
+    alert('Mr.National Geographic right?');
+    }else if (response2 === 'no' || response2 === 'n')
+  {
+    alert('Think again');
+  }else {
+    alert('Say What?');
+  }
+  console.log('Chika loves the outdoors!');
+  }
 
 answer5()
 
